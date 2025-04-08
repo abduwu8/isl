b8b2fbf23a970103bd35dfd47c80a8eb29f246e6
AbdullahOviEdu <abdullah.intern2025@oviedu.com>
2025-04-09 02:31:08 +0530

    ww

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['isl-dttk.onrender.com', '.onrender.com']
+  },
+  server: {
+    host: true
+  }
 })
